6eab0e007430319d491f8be5b2d9fa92ab30ee85
aniruthsenthilkumar <senthilkumaraniruth@gmail.com>
2023-09-13 20:59:15 -0500

    added a test database and a Python program to write to the database

---
+++ BackendServer/send2db.py
@@ -0,0 +1,23 @@
+import sqlite3
+
+try:
+    sqliteConnection = sqlite3.connect('test.db')
+    cursor = sqliteConnection.cursor()
+    print("Successfully Connected to SQLite")
+
+    sqlite_insert_query = """INSERT INTO BME688
+                          (Temperature, Humidity, Gas, Pressure) 
+                           VALUES 
+                          (22, 23, 21, 212)"""
+
+    count = cursor.execute(sqlite_insert_query)
+    sqliteConnection.commit()
+    print("Record inserted successfully into SqliteDb_developers table ", cursor.rowcount)
+    cursor.close()
+
+except sqlite3.Error as error:
+    print("Failed to insert data into sqlite table", error)
+finally:
+    if sqliteConnection:
+        sqliteConnection.close()
+        print("The SQLite connection is closed")
