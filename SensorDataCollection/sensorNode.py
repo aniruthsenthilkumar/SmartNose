d04980af90bd3bf54eb8954c3ae453752d60c53e
Smart Nose Sam <senthilkumaraniruth@gmail.com>
2023-09-16 21:09:17 -0500

    able to store sensor data into database

--- SensorDataCollection/sensorNode.py
+++ SensorDataCollection/sensorNode.py
@@ -1,4 +1,5 @@
 import board
+import sqlite3
 import adafruit_bme680
 i2c = board.I2C()
 sensor = adafruit_bme680.Adafruit_BME680_I2C(i2c)
@@ -10,4 +11,32 @@ print('Gas: {} ohms'.format(sensor.gas))
 print('Humidity: {}%'.format(sensor.humidity))
 print('Pressure: {}hPa'.format(sensor.pressure))
 
-# can edit on r pi
+temp = sensor.temperature
+gas = sensor.gas
+humid = sensor.humidity
+press = sensor.pressure
+
+"("
+
+try:
+    sqliteConnection = sqlite3.connect('localDatabase.db')
+    cursor = sqliteConnection.cursor()
+    print("Successfully Connected to SQLite")
+
+    sqlite_insert_query = """INSERT INTO BME688
+                          (Temperature, Humidity, Gas, Pressure) 
+                           VALUES 
+                          ({0}, {1}, {2}, {3})""".format(temp, humid, gas, press)
+
+    count = cursor.execute(sqlite_insert_query)
+    sqliteConnection.commit()
+    print("Record inserted successfully into BME688 table ", cursor.rowcount)
+    cursor.close()
+
+except sqlite3.Error as error:
+    print("Failed to insert data into BME688 table", error)
+finally:
+    if sqliteConnection:
+        sqliteConnection.close()
+        print("The SQLite connection is closed")
+
